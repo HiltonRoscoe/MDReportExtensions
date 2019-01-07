2947a92c5cc6ba79ec801d3f504bbb663dd1e481
John Dziurlaj <john@hiltonroscoe.com>
2019-01-07 15:39:33 -0500

    new example to match new parameter list

--- src/main/java/com/hiltonroscoe/mdreportext/Test.java
+++ src/main/java/com/hiltonroscoe/mdreportext/Test.java
@@ -1,10 +1,15 @@
 package com.hiltonroscoe.mdreportext;
+
+import java.util.ArrayList;
+
 /**
  * Harness for testing the JAR prior to deployment.
  */
 public class Test {
     public static void main(String[] args) {
-        String text = "Programmed device that creates credentials necessary to begin a voting session using a specific ballot style.";
-        System.out.println(NLPLanguage.runNLP(text, "ballot style", "mapping.txt"));
+        String text = "A set of marked ballots with a predetermined outcome. Used for logic and accuracy testing of a voting system.";
+        ArrayList<String> skipTerms = new ArrayList<String>();
+        skipTerms.add("marked ballot");
+        System.out.println(NLPLanguage.runNLP(text, "ballot style", "mapping.txt", skipTerms));
     }
 }
