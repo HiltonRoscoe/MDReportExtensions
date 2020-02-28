bed9da112879cd4ab333fcb4098bef205ec7a9e2
John Dziurlaj <john@hiltonroscoe.com>
2020-02-28 07:44:00 -0500

    handle particular attribute sort per NIST

--- src/main/kotlin/com/hiltonroscoe/mdreportext/Sorter.kt
+++ src/main/kotlin/com/hiltonroscoe/mdreportext/Sorter.kt
@@ -25,8 +25,49 @@ class Sorter : Tool() {
           val s2 = PropertyUtils.getProperty(o2, "name") as String
             return@Comparator s2.length - s1.length// comparision
         }
+<<<<<<< Updated upstream
         catch (e:Exception) {
            return@Comparator 0
+=======
+
+        /** Exposes Collections.reverse method */
+        @JvmStatic
+        fun reverse(list: List<*>) {
+            return Collections.reverse(list)
+        }
+
+        @JvmStatic
+                /**
+                 * Sorts attributes by their name, with special rules for NIST CDFs.
+                 * @param collection Collection of attributes
+                 * @return A sorted collection of attributes
+                 */
+        fun sortByRule(collection: ArrayList<*>): ArrayList<*> {
+            Collections.sort(collection, Comparator { o1, o2 ->
+                try {
+                    val normalizeString = { sInput: String ->
+                        if (sInput.startsWith("Other"))
+                            sInput.substringAfter("Other").toLowerCase() + " "
+                        else if (Regex("^End[A-Z]*").containsMatchIn(sInput))
+                            sInput.substringAfter("End").toLowerCase() + " "
+                        else if (Regex("^Start[A-Z]*").containsMatchIn(sInput))
+                            sInput.substringAfter("Start").toLowerCase()
+                        else if (sInput.endsWith("End"))
+                            sInput.substringBefore("End").toLowerCase() + "start "
+                        else
+                            sInput.toLowerCase()
+
+                    }
+                    val s1 = PropertyUtils.getProperty(o1, "name") as String
+                    val s2 = PropertyUtils.getProperty(o2, "name") as String
+                    return@Comparator normalizeString(s1).compareTo(normalizeString(s2))// comparision
+                } catch (e: Exception) {
+                    System.err.println(e.toString())
+                    return@Comparator 0
+                }
+            })
+            return collection
+>>>>>>> Stashed changes
         }
       })
       return collection
